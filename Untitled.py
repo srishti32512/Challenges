33d689b5bd90bdd2d201f4f80d72a3a93d641879
srishti32512 <158147743+srishti32512@users.noreply.github.com>
2024-01-30 01:01:52 -0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# In[5]:
+
+
+# Specify the file path to your Excel file
+file_path = "/Users/srishtisharma/Downloads/Health_Sample dataset_SBP by month.xlsx"
+
+
+# In[6]:
+
+
+# Read the data from the first data file
+df = pd.read_excel("Health_Sample dataset_SBP by month.xlsx")
+
+
+# In[ ]:
+
+
+
+
